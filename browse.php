627edaf2cf80387b9b6904d0b3f9fe99c7335c24
linus97johansson <linus97johansson@gmail.com>
2017-11-02 21:29:17 +0100

    jk

--- browse.php
+++ browse.php
@@ -46,42 +46,37 @@
                     #after that it is wise to use addslashes, it adds slashes if there's an apostrophe or quotation mark
                     $searchQuerry = addslashes($searchQuerry);
 
-                    var_dump($searchQuerry);
+//                    var_dump($searchQuerry);
 
                     #here we create a variable $id and basically say that we want the data from the array matching the search criteria
-                    $id = array_search($searchQuerry, array_column($books, 'title'));
-                    $id2 = array_search($searchQuerry, array_column($books, 'author'));
+//                    $id = array_search($searchQuerry, array_column($books, 'title'));
+//                    $id2 = array_search($searchQuerry, array_column($books, 'author'));
 
-                    $sql = " SELECT * from books";
+                    $sql = " SELECT * FROM `books` WHERE `title` LIKE '%".$searchQuerry."%' OR `author` LIKE '%".$searchQuerry."%'
+";
                     $stmt = $dbh->prepare($sql);
                     $stmt->execute();
                     $books= $stmt->fetchAll();
 
+                    $hits = count($books)
+                    ;
+//                    var_dump($books['0']['bookid']);
 
                     #now we check if we have the ID or not in our array. If the search was a hit, it will assign something to our DB, if not, then it will not work.
-                    if ($id !== FALSE) {
-                        $book = $books[$id];
-                        $title = $book['title'];
-                        $author = $book['author'];
-                        print'
+                    if ($books !== FALSE) {
+                        foreach ($books as $book) {
+                            $id = $book['bookid'];
+                            $title = $book['title'];
+                            $author = $book['author'];
+                            print'
                 <div class="content big">
                     <img src="images/book.png" alt="cover"> <span>Title:' . $title . '. Author:' . $author . '.</span>
-                    <a href="reserve.php?reservation=' .  urlencode($title) . '"> Reserve </a>
+                    <a href="reserve.php?reservation=' . urlencode($title) . '"> Reserve </a>
                 </div>
                 ';
+                        }
                     }
-                    if ($id2 !== FALSE) {
-                        $book = $books[$id2];
-                        $title = $book['title'];
-                        $author = $book['author'];
-                        print'
-                <div class="content big">
-                    <img src="images/book.png" alt="cover"> <span>Title:' . $title . '. Author:' . $author . '.</span>
-                    <a href="reserve.php?reservation=' .  urlencode($title) . '"> Reserve </a>
-                </div>
-                ';
-                    }
-                    if ($id == FALSE && $id2 == FALSE){
+                    if ($books == FALSE){
                         print'There were no books matching your search!';
                     }
                 }
