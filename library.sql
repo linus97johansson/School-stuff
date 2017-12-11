7458d2bf2bdf661d8177d4fd1a36d903029f6625
linus97johansson <linus97johansson@gmail.com>
2017-12-11 13:43:08 +0100

    sr

--- library.sql
+++ library.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 22 nov 2017 kl 10:45
--- Serverversion: 10.1.26-MariaDB
--- PHP-version: 7.1.8
+-- Tid vid skapande: 11 dec 2017 kl 13:42
+-- Serverversion: 10.1.21-MariaDB
+-- PHP-version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,6 +23,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabellstruktur `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `password`) VALUES
+(1, 'Linus134', '8dc9a5b7f19590986715b93f8929eb0a6ec0c5a0');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `authors`
+--
+
+CREATE TABLE `authors` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `socialSecurity` varchar(255) DEFAULT NULL,
+  `birthYear` date DEFAULT NULL,
+  `externalSite` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `authors`
+--
+
+INSERT INTO `authors` (`id`, `firstName`, `lastName`, `socialSecurity`, `birthYear`, `externalSite`) VALUES
+(1, 'J.K.', 'Rowling', '0000', '1965-07-31', 'https://www.jkrowling.com/'),
+(2, 'William', 'Shakespeare', '000', '1564-01-01', 'https://sv.wikipedia.org/wiki/William_Shakespeare');
+
+-- --------------------------------------------------------
+
+--
 -- Tabellstruktur `books`
 --
 
@@ -32,7 +72,7 @@ CREATE TABLE `books` (
   `bookid` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `author` varchar(100) NOT NULL,
-  `reserved` tinyint(1) DEFAULT NULL,
+  `reserved` tinyint(1) DEFAULT '0',
   `duedate` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -45,9 +85,40 @@ INSERT INTO `books` (`bookid`, `title`, `author`, `reserved`, `duedate`) VALUES
 (2, 'Othello', 'William Shakespeare', 1, '2013-11-29'),
 (3, 'Pilgrim\'s Progress', 'John Bunyan', 0, NULL);
 
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `connections`
+--
+
+CREATE TABLE `connections` (
+  `authorID` int(255) NOT NULL,
+  `bookID` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `connections`
+--
+
+INSERT INTO `connections` (`authorID`, `bookID`) VALUES
+(1, 1),
+(2, 2);
+
 --
 -- Index för dumpade tabeller
 --
+
+--
+-- Index för tabell `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index för tabell `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index för tabell `books`
@@ -56,14 +127,41 @@ ALTER TABLE `books`
   ADD PRIMARY KEY (`bookid`);
 
 --
+-- Index för tabell `connections`
+--
+ALTER TABLE `connections`
+  ADD PRIMARY KEY (`authorID`),
+  ADD KEY `connections_ibfk_1` (`bookID`);
+
+--
 -- AUTO_INCREMENT för dumpade tabeller
 --
 
 --
+-- AUTO_INCREMENT för tabell `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT för tabell `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT för tabell `books`
 --
 ALTER TABLE `books`
-  MODIFY `bookid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
+  MODIFY `bookid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Restriktioner för dumpade tabeller
+--
+
+--
+-- Restriktioner för tabell `connections`
+--
+ALTER TABLE `connections`
+  ADD CONSTRAINT `connections_ibfk_1` FOREIGN KEY (`bookID`) REFERENCES `books` (`bookid`),
+  ADD CONSTRAINT `connections_ibfk_2` FOREIGN KEY (`authorID`) REFERENCES `authors` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
